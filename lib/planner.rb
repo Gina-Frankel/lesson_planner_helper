a1aa4285ff483a4b0f58c106c8e65d189ae2b4a5
Gina Frankel <g.frankel22@gmail.com>
2020-06-19 19:28:11 +0100

    Add: add method

--- lib/planner.rb
+++ lib/planner.rb
@@ -4,6 +4,10 @@ class Planner
   end
 
   def get_welcome_message
-    'Welcome to the Teaching Planning helper'
+    'Welcome to the Provocation Station Planning Tool'
   end
+
+  def add(resource_item) 
+    "pencils"
+  end 
 end
