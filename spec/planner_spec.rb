6a8b77a9f01ecf1eed04b4347a9526a9094a1c76
Gina Frankel <g.frankel22@gmail.com>
2020-06-19 18:20:37 +0100

    Red: get_welcome_message returns a welcome message

--- spec/planner_spec.rb
+++ spec/planner_spec.rb
@@ -1,13 +1,12 @@
 require 'planner'
 
 describe Planner do
-  it "returns a welcome message" do
-    planner = Planner.new
-    welcome_message = 'Welcome to the Teaching Planning helper'
+  describe "#get_welcome_message" do 
+    it "returns a welcome message" do
+      planner = Planner.new
+      welcome_message = 'Welcome to the Teaching Planning helper'
 
-    expect(planner.get_welcome_message).to eq welcome_message
+      expect(planner.get_welcome_message).to eq welcome_message
+    end 
   end 
-
-it 'returns '
-
-end
+end 
