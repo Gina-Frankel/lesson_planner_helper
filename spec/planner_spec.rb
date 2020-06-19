948c03813f59a0f3b5085ccb9bded7bec4c1260e
Gina Frankel <g.frankel22@gmail.com>
2020-06-19 19:27:31 +0100

    Red: add method - adds resources

--- spec/planner_spec.rb
+++ spec/planner_spec.rb
@@ -4,9 +4,18 @@ describe Planner do
   describe "#get_welcome_message" do 
     it "returns a welcome message" do
       planner = Planner.new
-      welcome_message = 'Welcome to the Teaching Planning helper'
+      welcome_message = 'Welcome to the Provocation Station Planning Tool'
 
       expect(planner.get_welcome_message).to eq welcome_message
+    end 
+  end 
+
+    describe '#add' do
+      it 'adds resources' do
+        planner = Planner.new 
+        
+
+        expect(planner.add("pencils")).to eq "pencils"
     end 
   end 
 end 
