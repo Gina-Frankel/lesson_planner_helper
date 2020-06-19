177868e7978313a2048144ba5aa65ddd873c9d8c
Gina Frankel <g.frankel22@gmail.com>
2020-06-19 22:00:51 +0100

    Lint with prettier

--- spec/planner_spec.rb
+++ spec/planner_spec.rb
@@ -1,27 +1,29 @@
+# frozen_string_literal: true
+
 require 'planner'
 
 describe Planner do
-  describe "#get_welcome_message" do 
-    it "returns a welcome message" do
+  describe '#get_welcome_message' do
+    it 'returns a welcome message' do
       planner = Planner.new
       welcome_message = 'Welcome to the Provocation Station Planning Tool'
 
       expect(planner.get_welcome_message).to eq welcome_message
-    end 
-  end 
+    end
+  end
 
-    describe '#add' do
-      it 'adds a resource' do
-        planner = Planner.new 
-        
-        expect(planner.add_resource("Pencils")).to eq "Pencils"
-    end 
+  describe '#add' do
+    it 'adds a resource' do
+      planner = Planner.new
 
-      it 'adds multiple resources' do 
-        planner = Planner.new 
+      expect(planner.add_resource('Pencils')).to eq 'Pencils'
+    end
 
-        planner.add_resource("Pencils")
-        expect(planner.add_resource("Paint")).to eq "Pencils\nPaint"
-    end 
-  end 
-end 
+    it 'adds multiple resources' do
+      planner = Planner.new
+
+      planner.add_resource('Pencils')
+      expect(planner.add_resource('Paint')).to eq "Pencils\nPaint"
+    end
+  end
+end
