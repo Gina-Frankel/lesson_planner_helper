0da762823ff9797af032cc617cbb0166a8e081a7
Gina Frankel <g.frankel22@gmail.com>
2020-06-20 21:36:06 +0100

    Fix name of method

--- spec/feature_test_spec.rb
+++ spec/feature_test_spec.rb
@@ -1,11 +1,11 @@
 require 'lesson_planner'
 
-xdescribe 'Feature test:' do
+describe 'Feature test:' do
     it 'user can add a resource to plan' do
       lesson_planner = LessonPlanner.new
       resource_planner = ResourcePlanner.new
 
-      expect{ lesson_planner.add(resource_planner,'Pencils') }.to output("Resources:\nPencils\nPaint").to_stdout
+      expect{ lesson_planner.write(resource_planner,'Pencils') }.to output("Resources:\nPencils\nPaint").to_stdout
     end
 
     
