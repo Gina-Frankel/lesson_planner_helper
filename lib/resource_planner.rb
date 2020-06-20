b76842cc900b6b0a7c319359fb59660fc71f46b1
Gina Frankel <g.frankel22@gmail.com>
2020-06-20 21:41:30 +0100

    Lint add space after private

--- lib/resource_planner.rb
+++ lib/resource_planner.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class ResourcePlanner
   attr_reader :heading, :resources
   def initialize
@@ -8,11 +10,11 @@ class ResourcePlanner
   def add_to_plan(user_input)
     resources.push(user_input)
     resources.join("\n")
-  end 
+  end
 
   private
+
   def convert_to_new_line_seperated_value
     resources.join("\n")
-  end 
-
+  end
 end
