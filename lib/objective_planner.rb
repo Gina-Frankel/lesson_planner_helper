cb2f73178822d2a2097000e970f55b7934bd615c
Gina Frankel <g.frankel22@gmail.com>
2020-06-22 12:55:07 +0100

    Add method to add onr numbered resource to array

--- lib/objective_planner.rb
+++ lib/objective_planner.rb
@@ -1,5 +1,17 @@
 class ObjectivePlanner
+
+ attr_reader :objectives
   def initialize
-    
+    @objectives =["Objectives:"]
   end
+
+  def add_to_plan(user_input)
+    objectives.push('1. ' + user_input)
+    convert_to_new_line_seperated_value
+  end
+
+  def convert_to_new_line_seperated_value
+    objectives.join("\n")
+  end
+
 end
--- spec/objective_planner_spec.rb
+++ spec/objective_planner_spec.rb
@@ -1,5 +1,11 @@
 require 'objective_planner'
 
 describe ObjectivePlanner do
-  
+  it 'adds one numbered resource to array' do
+    objective_planner = ObjectivePlanner.new
+
+    Subheading = "Objectives:"
+    objective = "To add one digit numbers"
+    expect(objective_planner.add_to_plan(objective)).to eq "#{Subheading}\n1. #{objective}"
+  end
 end
