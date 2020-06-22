80628dc5b5b5b7a9917f59f4c2c406257e62b077
Gina Frankel <g.frankel22@gmail.com>
2020-06-22 14:30:53 +0100

    Refactor include header in objectives array

--- lib/objective_planner.rb
+++ lib/objective_planner.rb
@@ -2,15 +2,14 @@ class ObjectivePlanner
 
  attr_reader :objectives, :objective_number
  HEADER = "Objectives:"
+
   def initialize
-    @objectives =[]
-    @objective_number = 0
+    @objectives =[[HEADER]]
   end
 
   def add_to_plan(user_input)
-    @objective_number += 1
-    objective_number = objectives.length + 1
-    numbered_plan = objective_number.to_s + '. ' + user_input
+    objective_number = objectives.length.to_s
+    numbered_plan = objective_number + '. ' + user_input
     objectives.push([numbered_plan])
     format_for_display
   end
@@ -18,7 +17,7 @@ class ObjectivePlanner
   private
 
   def format_for_display
-    HEADER + "\n" + objectives.join("\n")
+     objectives.join("\n")
   end
 
 end
