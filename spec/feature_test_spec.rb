b805835e6f7826b876e4390a01c4dada0b0e6642
Gina Frankel <g.frankel22@gmail.com>
2020-06-22 12:07:32 +0100

    Add feature test - user can add objectives to plan (failing)

--- spec/feature_test_spec.rb
+++ spec/feature_test_spec.rb
@@ -11,5 +11,15 @@ describe 'Feature test:' do
       expect{ lesson_planner.display }.to output("Resources:\nPencils\nPaint").to_stdout
     end
 
+    it 'user can add an objective to a plan' do
+      lesson_planner = LessonPlanner.new
+      objective_planner = ObjectivePlanner.new
+
+      lesson_planner.write(objective_planner,'1. To write their name')
+      lesson_planner.write(objective_planner,'2. To identify the letters of thier name')
+
+      expect{ lesson_planner.display }.to output("Objective:\nTo write their name\nTo identify the letters of thier name").to_stdout
+    end
+
     
   end
