3aa5c319529d35f06ddbe860164a62021e1b0c5f
Gina Frankel <g.frankel22@gmail.com>
2020-06-22 15:05:46 +0100

    Add method that adds a numbered question to array of arrays

--- lib/question_planner.rb
+++ lib/question_planner.rb
@@ -1,5 +1,22 @@
+# frozen_string_literal: true
+
 class QuestionPlanner
+  attr_reader :questions
+
+  HEADER = 'Questions:'
   def initialize
-    
+    @questions = [[HEADER]]
+  end
+
+  def add_to_plan(user_input)
+    question_number = questions.length.to_s
+    numbered_questions = question_number + '. ' + user_input
+    questions.push([numbered_questions])
+    format_for_display
+  end
+
+  def format_for_display
+    pp questions
+    questions.join("\n")
   end
 end
--- spec/question_planner_spec.rb
+++ spec/question_planner_spec.rb
@@ -1,4 +1,10 @@
 require 'question_planner'
 describe QuestionPlanner do
-  
+  it 'adds a numbered question as an array to an array' do
+    question_planner = QuestionPlanner.new
+
+    HEADER = "Questions:"
+    objective = "What else can you add?"
+    expect(question_planner.add_to_plan(objective)).to eq "#{HEADER}\n1. #{objective}"
+  end
 end
